131e9cc348e1a84734dddb436ca776a268034a44
madeleinewhitehall <maddiewhitehall@Maddies-MBP-2.lan>
2017-03-05 23:49:48 +0000

    Forgot I changed the db_connect file

--- htdocs/db_connect.php
+++ htdocs/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$dbc = mysqli_connect('localhost','root','P1zzaRat3013', 'sn_coursework')
+$dbc = mysqli_connect('localhost','root','P1zzaRat3013','sn_coursework')
 OR die(mysqli_connect_error());
 mysqli_set_charset($dbc,'utf-8');
 
