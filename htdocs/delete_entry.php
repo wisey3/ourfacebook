11986c99189f74dd10ca8155f366018ae66e5ffc
AmandaJBell <amandajbell94@gmail.com>
2017-03-14 12:16:23 +0000

    Update delete_entry.php
    update blog to use ajax on this request

--- htdocs/delete_entry.php
+++ htdocs/delete_entry.php
@@ -1,7 +1,9 @@
 <?php
   include 'db_connect.php';
   //if they submitted the form
-  if (isset($_POST['submit'])) {
+ $errors = array(); //To store errors
+$form_data = array(); 
+header('Content-Type: application/json');
     //escape special characters from user input
     $id = mysqli_real_escape_string($dbc, $_POST['post_id']);
     //otherwise
@@ -9,14 +11,26 @@
       //create a query that inserts the messages
       $query = "DELETE FROM posts WHERE id = '$id'";
       //send query in and if it doesn't work report an error
+
+
+      if (!empty($errors)) { //If errors in validation
+    $form_data['success'] = false;
+    $form_data['errors']  = $errors;
+} 
+    //otherwise
+    else {
       if (!mysqli_query($dbc, $query)) {
-        die('Error: ' . mysqli_error($connection));
+         die('Error: ' . mysqli_error($dbc));
+        $errors['failed'] = 'delete post failed';
+        $form_data['success'] = false;
+      $form_data['errors']  = $errors;
       }
       else {
         //returns an http header
-        header('Location: profile.php');
-        exit();
+        $form_data['success'] = true;
+      $form_data['posted'] = 'delete post Successful';
       }
+    }
     
-  }
+     echo json_encode($form_data);  
 ?>
